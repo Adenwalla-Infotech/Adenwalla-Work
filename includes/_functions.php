3d03e6bbca2369c0f99aafccd301c2ece0d9c679
Juned Adenwalla <adenwalla.juned@gmail.com>
2022-11-27 19:09:17 +0530

    rtrtetertertet

--- includes/_functions.php
+++ includes/_functions.php
@@ -750,49 +750,48 @@ function _notifyuser($useremail = '', $userphone = '', $sendmail = '', $message 
             }
         }
     }
-    // if ($useremail != '') {
-    //     $sql = "SELECT * FROM `tblemailconfig` WHERE `_supplierstatus` = 'true'";
-    //     $query = mysqli_query($conn, $sql);
-    //     $count = mysqli_num_rows($query);
-    //     if ($count == 1) {
-    //         require_once "../vendor/autoload.php";
-    //         $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-    //         //Set PHPMailer to use SMTP.
-    //         $mail->isSMTP();
-    //         foreach ($query as $data) {
-    //             //Enable SMTP debugging.
-    //             // $mail->SMTPDebug = 10;                                       
-    //             //Set SMTP host name                          
-    //             $mail->Host = $data['_hostname'];
-    //             //Set this to true if SMTP host requires authentication to send email
-    //             $mail->SMTPAuth = $data['_smtpauth'];
-    //             //Provide username and password     
-    //             $mail->Username = $data['_emailaddress'];
-    //             $mail->Password = $data['_emailpassword'];
-    //             //If SMTP requires TLS encryption then set it
-    //             $mail->SMTPSecure = "ssl";
-    //             //Set TCP port to connect to
-    //             $mail->Port = $data['_hostport'];
+    if ($useremail != '') {
+        $sql = "SELECT * FROM `tblemailconfig` WHERE `_supplierstatus` = 'true'";
+        $query = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($query);
+        if ($count == 1) {
+            require_once "../vendor/autoload.php";
+            $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+            //Set PHPMailer to use SMTP.
+            $mail->isSMTP();
+            foreach ($query as $data) {
+                //Enable SMTP debugging.
+                // $mail->SMTPDebug = 10;                                       
+                //Set SMTP host name                          
+                $mail->Host = $data['_hostname'];
+                //Set this to true if SMTP host requires authentication to send email
+                $mail->SMTPAuth = $data['_smtpauth'];
+                //Provide username and password     
+                $mail->Username = $data['_emailaddress'];
+                $mail->Password = $data['_emailpassword'];
+                //If SMTP requires TLS encryption then set it
+                $mail->SMTPSecure = "ssl";
+                //Set TCP port to connect to
+                $mail->Port = $data['_hostport'];
 
-    //             $mail->From = $data['_emailaddress'];
-    //             $mail->FromName = $data['_sendername'];
-    //         }
-    //         //To address and namS
-    //         $mail->addAddress($useremail); //Recipient name is optional
+                $mail->From = $data['_emailaddress'];
+                $mail->FromName = $data['_sendername'];
+                //Address to which recipient will reply
+                $mail->addReplyTo($data['_emailaddress'], "Reply");
+            }
+            //To address and namS
+            $mail->addAddress($useremail); //Recipient name is optional
 
-    //         //Address to which recipient will reply
-    //         $mail->addReplyTo($data['_emailaddress'], "Reply");
+            $mail->isHTML(true);
 
-    //         $mail->isHTML(true);
-
-    //         $mail->Subject = $subject;
-    //         $mail->Body = $sendmail;
-    //         $mail->IsHTML(true);
-    //         if ($mail->send()) {
-    //             $_SESSION['send_mail'] = true;
-    //         }
-    //     }
-    // }
+            $mail->Subject = $subject;
+            $mail->Body = $sendmail;
+            $mail->IsHTML(true);
+            if ($mail->send()) {
+                $_SESSION['send_mail'] = true;
+            }
+        }
+    }
 }
 
 function _getuser($username = '', $usertype = '', $limit = '', $startfrom = '')
